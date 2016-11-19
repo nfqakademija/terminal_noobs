b6bee08bf0690e9855ff8af456b898f654e1aaa3
Matas Minelga <minematas@gmail.com>
2016-11-19 15:38:45 +0200

    Added Admin view

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matas
+ * Date: 16.11.19
+ * Time: 13.48
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller
+{
+
+    /**
+     * @Route("/admin/overview")
+     */
+    public function overviewAction()
+    {
+        return $this->render('AppBundle:Admin:overview.html.twig', array(
+            // ...
+        ));
+    }
+}
---
+++ src/AppBundle/Repository/AdminRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matas
+ * Date: 16.11.19
+ * Time: 14.23
+ */
+
+namespace AppBundle\Repository;
+
+
+class AdminRepository extends \Doctrine\ORM\EntityRepository
+{
+
+
+}
