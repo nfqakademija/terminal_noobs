35bb646a295e3626ab6cac53505dd243145f6d82
Matas Minelga <minematas@gmail.com>
2016-11-24 16:52:58 +0200

    Fixed all things with administrator overview.

--- src/AppBundle/Resources/scripts/widgets/teamList.js
+++ src/AppBundle/Resources/scripts/widgets/teamList.js
@@ -3,26 +3,22 @@ $(function () {
     var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    var btn = document.getElementById("");
 
 // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+    $("#myBtn").on('click', function() {
+        $("#myModal").show();
+    });
 
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+    $(".close").on('click', function() {
+        $("#myModal").hide();
+    });
 
-// When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
+    $('body').on('click', function(e) {
         if (event.target == modal) {
-            modal.style.display = "none";
+            $("#myModal").hide();
         }
-    }
-
+    });
 });
