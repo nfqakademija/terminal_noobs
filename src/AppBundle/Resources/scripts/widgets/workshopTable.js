056d12e077650136a19f30419c35ad6d2ef56e33
Viktorija Razaite <viktorija.razaite@gmail.com>
2016-11-30 16:32:03 +0200

    all users for admin

---
+++ src/AppBundle/Resources/scripts/widgets/workshopTable.js
@@ -0,0 +1,38 @@
+
+$(function () {
+    /*marks for team*/
+    /*
+    $("#label").bind("keyup", changed).bind("change", changed);
+
+    function changed() {
+        $("#url").val(this.value);
+    }
+    */
+
+
+    /*save input*/
+    /*
+    var timeoutId;
+    $('#the-textarea').on('input propertychange change', function() {
+        console.log('Textarea Change');
+
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(function() {
+            // Runs 1 second (1000 ms) after the last change
+            saveToDB();
+        }, 1000);
+    });
+
+    function saveToDB()
+    {
+        console.log('Saving to the db');
+    }
+
+    // This is just so we don't go anywhere
+    // and still save if you submit the form
+    $('.contact-form').submit(function(e) {
+        saveToDB();
+        e.preventDefault();
+    });
+    */
+});
