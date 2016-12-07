57601e4758db612f1efa736e574b575234f4877e
Viktorija Razaite <viktorija.razaite@gmail.com>
2016-12-07 23:53:42 +0200

    changed max lenght in input

--- src/AppBundle/Resources/scripts/widgets/workshopTable.js
+++ src/AppBundle/Resources/scripts/widgets/workshopTable.js
@@ -6,29 +6,9 @@ $(function () {
         $(".js-team-" + $(this).data('team-name')).val($(this).val());
     });
 
-    /*save input*/
-    /*
-    var timeoutId;
-    $('#the-textarea').on('input propertychange change', function() {
-        console.log('Textarea Change');
-
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(function() {
-            // Runs 1 second (1000 ms) after the last change
-            saveToDB();
-        }, 1000);
+    $('input[name="no1"]').keypress(function() {
+        if (this.value.length >= 2) {
+            return false;
+        }
     });
-
-    function saveToDB()
-    {
-        console.log('Saving to the db');
-    }
-
-    // This is just so we don't go anywhere
-    // and still save if you submit the form
-    $('.contact-form').submit(function(e) {
-        saveToDB();
-        e.preventDefault();
-    });
-    */
 });
